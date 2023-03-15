021be162e307bf31dcafe5558f266b04bf56f9b5
uzukhanye Dywili <dywiliuzukhanye@gmail.com>
2023-03-15 16:51:50 +0200

    update

--- config/index.js
+++ config/index.js
@@ -1,13 +1,13 @@
 import connect from 'dotenv'
-import {createPool} from 'mysql'
+import {createPool} from 'mysql2'
 
 const dotenv = connect
 dotenv.config();
 
 const connection = createPool({
-    host: process.env.HOST,
+    host: process.env.dbHost,
     user: process.env.dbUser,
-    password: process.env.dbPass,
+    password: process.env.dbPwd,
     port: process.env.dbPort,
     database: process.env.dbName,
     multipleStatements: true
